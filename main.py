ded6ecb13ab317de1e28195f4a92e7b6f348533f
tnovak123 <timothy.m.novak@gmail.com>
2018-04-09 17:46:12 -0500

    Changed fuctionality for Delete. Added task completed field.

--- main.py
+++ main.py
@@ -13,9 +13,11 @@ class Task(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120))
+    completed = db.Column(db.Boolean)
 
     def __init__(self, name):
         self.name = name
+        self.completed = False
 
 
 
@@ -29,19 +31,21 @@ def index():
         db.session.add(new_task)
         db.session.commit()
 
-    tasks = Task.query.all()
-
-    return render_template('todos.html',title="Get It Done!", tasks=tasks)
+    tasks = Task.query.filter_by(completed=False).all()
+    completed_tasks = Task.query.filter_by(completed=True).all()
+    
+    return render_template('todos.html',title="Get It Done!", tasks=tasks, completed_tasks=completed_tasks)
 
 @app.route('/delete-task', methods=['POST'])
 def delete_task():
 
     task_id = int(request.form['task-id'])
     task = Task.query.get(task_id)
-    db.session.delete(task)
+    db.completed = True
+    db.session.add(task)
     db.session.commit()
 
-    return redirect('')
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run()
