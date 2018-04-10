cf562103e7c742ce42ae13de10a3d2a7badb71ca
tnovak123 <timothy.m.novak@gmail.com>
2018-04-10 18:56:43 -0500

    Updated owner coding.

--- main.py
+++ main.py
@@ -15,16 +15,19 @@ class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120))
     completed = db.Column(db.Boolean)
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __init__(self, name):
+    def __init__(self, name, owner):
         self.name = name
         self.completed = False
+        self.owner = owner
 
 class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(120))
+    tasks = db.relationships('Task', backref='owner')
 
     def __init__(self, email, password):
         self.email = email
@@ -82,6 +85,8 @@ def logout():
 @app.route('/', methods=['POST', 'GET'])
 def index():
 
+    owner = User.query.filter_by(email=session['email']).first
+    
     if request.method == 'POST':
         task_name = request.form['task']
         new_task = Task(task_name)
